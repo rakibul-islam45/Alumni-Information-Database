2e08bfc8d97181e5a8295def14f69f45c289b9e4
rakibul-islam45 <zero.rakibul@gmail.com>
2022-08-26 23:42:32 +0600

    Login Functionality added

---
+++ Alumni_Info/Login.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Alumni_Info.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="background-color:lightcyan;width : 100%; height: 100%;">
+        <br/>
+        <br/>
+    <center>
+    <table style="width:100%;">
+        <tr>
+            <td style="height: 22px; width: 285px">&nbsp;</td>
+            <td style="height: 22px; width: 227px;"></td>
+            <td style="height: 22px"></td>
+        </tr>
+        <tr>
+            <td style="width: 285px" >&nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="Label1" runat="server" Text="UserName" Font-Bold="True"></asp:Label>
+            </td>
+            <td style="width: 227px">
+                <asp:TextBox ID="luser" runat="server" Width="169px" Height="20px"></asp:TextBox>
+            </td>
+            <td>
+                <asp:Label ID="usrpass" runat="server" Text="Login as Admin or User!"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 285px">&nbsp;&nbsp;&nbsp;&nbsp; <asp:Label ID="Label2" runat="server" Text="Password" Font-Bold="True"></asp:Label></td>
+            <td style="width: 227px">
+                <asp:TextBox ID="lpassword" runat="server" Type="Password" Height="24px" OnTextChanged="TextBox2_TextChanged" Width="168px"></asp:TextBox>
+            </td>
+            <td>
+                <asp:Label ID="errorx" runat="server" Text=""></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 285px">
+                
+                &nbsp;</td>
+            <td style="width: 227px">
+
+                <strong>
+                
+                <asp:Button ID="Login" runat="server" Text="Login" OnClick="Login_Click" Font-Bold="True" />
+                
+                </strong>
+
+            </td>
+            </tr>
+    </table>
+         </center>
+         <br/>
+       
+        <br/>
+    </div>
+</asp:Content>
---
+++ Alumni_Info/Login.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Security;
+
+namespace Alumni_Info
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+            "Data Source=DESKTOP-1L51OAC\\SQLEXPRESS;" +
+            "Initial Catalog=Alumni;" +
+            "Integrated Security=SSPI;";
+            conn.Open();
+
+            string usr = luser.Text;
+            string pass = lpassword.Text;
+            string encryptedPassword = FormsAuthentication.
+           HashPasswordForStoringInConfigFile(lpassword.Text, "SHA1");
+
+            SqlDataReader dataReader;
+
+            string sql = "SelECT username,password from register1 where username =" + "'" + usr + "'";
+
+            SqlCommand command;
+
+            command = new SqlCommand(sql, conn);
+
+            dataReader = command.ExecuteReader();
+
+            string x ="", xx ="";
+
+            while (dataReader.Read())
+            {
+                x = (dataReader.GetValue(0).ToString());
+                xx = dataReader.GetValue(1).ToString();
+            }
+            // Response.Write(x + " " + xx);
+            if (x == usr && xx == encryptedPassword)
+            {
+                Response.Write("Login Succesful");
+
+                //usrpass.Text = usr + " " + pass;
+               
+                Response.Redirect("User.aspx?UserName=" + luser.Text);
+
+            }
+             if (usr == "admin" && pass== "admin"){
+                Response.Write("<script>alert('Admin logged In!');window.location = 'AdminView.aspx';</script>");
+                //Server.Transfer("AdminView.aspx",true);
+            }
+            else
+            {
+                errorx.Text = "USER NAME OR PASSWORD IS WRONG";
+            }
+
+            conn.Close();
+        }
+
+        protected void luser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
